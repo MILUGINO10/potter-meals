fe0d6fcfae2f6f38bbbc4f38196a10f67abfa4d3
MILUGINO10 <116041186+MILUGINO10@users.noreply.github.com>
2022-12-14 20:17:55 -0300

    tp 8

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+function oscuro() {
+    console.log("oscuro") 
+    document.body.style.backgroundColor = "black";
+    document.body.style.backgroundImage = "none";
+    document.body.style.color ="white";
+}
+function cartel() {
+    alert ("Delgado Maria de los Milagros S.A Rodiguez peña 300, piso 2, barrio centenario, Santa Fe, CP: 3000")
+    }
